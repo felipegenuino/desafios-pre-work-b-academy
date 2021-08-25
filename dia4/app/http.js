c545320a944865d144894ddcb8ac1728fea3620a
Felipe Genuino <design@felipegenuino.com>
2021-08-25 07:20:54 -0300

    Resolve exercice 4

--- dia4/app/http.js
+++ dia4/app/http.js
@@ -1,15 +1,16 @@
 const request = (url, options) =>
   fetch(url, options)
-  .then(r=>r.json())
-  .catch(e=> ({error: true, message: e.message}))
+    .then(r => r.json())
+    .catch(e => ({ error: true, message: e.message }))
 
-  export const get = (url) => request(url)
-  export const post = (url, data) => request(url, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(data)
-  })
-  export const del = () => {}
+const createRequest = (method) => (url, data) => request(url, {
+  method,
+  headers: {
+    'content-type': 'application/json',
+  },
+  body: JSON.stringify(data)
+})
 
+export const get = (url) => request(url)
+export const post = createRequest('POST')
+export const del = createRequest('DELETE')
